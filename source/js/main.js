aa8b6b4f44beca55a25e77420a120bee3a4b1be6
NatalyLu <natulya.lukashina@bk.ru>
2022-05-13 01:07:11 +0300

    started work with header(mobile+tablet styles)

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+let burger = document.querySelector(".main-nav__burger");
+let navigation = document.querySelector(".main-nav");
+
+let toggleMenu = (evt, button, nav) => {
+	evt.preventDefault();
+	button.classList.toggle("button-burger--open");
+	nav.classList.toggle("main-nav--menu-open")
+	let attributeValue;
+
+	button.classList.contains("button-burger--open")
+		?
+			(attributeValue = "false")
+		:
+			(attributeValue = "true");
+
+	button.setAttribute("aria-expanded", attributeValue);
+}
+
+burger.addEventListener("click", (evt) => {
+  toggleMenu(evt, burger, navigation);
+});
